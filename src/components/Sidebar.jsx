07f5c501561d6d52ec6c4938db63feb10339552b
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-19 07:48:00 +0900

    Sidebarコンポーネントの内容を定義

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,22 @@ import "./Sidebar.css";
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div className='app-sidebar'>
+      <div className='app-sidebar-header'>
+        <h1>Note</h1>
+        <button>Add</button>
+      </div>
+      <div className='app-sidebar-notes'>
+        <div className='app-sidebar-note'>
+          <div className='sidebar-note-title'>
+            <strong>Title</strong>
+            <button>Delete</button>
+          </div>
+          <p>content</p>
+          <small>Upadated: xx/xx</small>
+        </div>
+      </div>
+    </div>
   );
 };
 
