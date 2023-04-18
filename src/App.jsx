9288bdb412f3ec35d7ec6482a3bc668a495f155b
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-19 07:43:38 +0900

    SidebarとMainのコンポーネントを作成して、App.jsx内にてimport

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import Main from './components/Main'
+import Sidebar from './components/Sidebar'
 
 function App() {
 
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Sidebar />
+      <Main />
+    </div>
   )
 }
 
