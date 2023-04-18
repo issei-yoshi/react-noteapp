ac572605db1bfa2e537d09095eafa9b08c2a1632
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-19 08:46:25 +0900

    Main.jsxにhtmlを記載

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,16 @@ import "./Main.css";
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div className='app-main'>
+      <div className='app-main-note-edit'>
+        <input type="text" />
+        <textarea id="" placeholder='write down note'></textarea>
+      </div>
+      <div className='app-main-note-preview'>
+        <h1 className='preview-title'>title</h1>
+        <div className='markdown-preview'>content</div>
+      </div>
+    </div>
   );
 };
 
